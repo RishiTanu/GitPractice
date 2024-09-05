1d7546f472996a7eba600aa563dc939544087da2
RishiTanu <singhrishikesh500@gmail.com>
2024-09-06 00:43:52 +0530

    fifth commit

--- app/src/main/java/com/example/gitpractice/MainActivity.kt
+++ app/src/main/java/com/example/gitpractice/MainActivity.kt
@@ -44,3 +44,34 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GitPracticeTheme {
+        Greeting("Android")
+    }
+}
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GitPracticeTheme {
+        Greeting("Android")
+    }
+}
